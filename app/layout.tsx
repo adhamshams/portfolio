5aba68f566348e55f7b2366c09dd637084e9faca
Adham <adham20011@gmail.com>
2025-01-16 11:59:17 +0200

    Finished homepage intro

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,48 @@
 import type { Metadata } from "next";
 import { SpeedInsights } from "@vercel/speed-insights/next"
-import { DM_Mono } from "next/font/google";
+import { DM_Mono, Barriecito, Delius_Swash_Caps, Eagle_Lake, Gloria_Hallelujah, Jost, Barrio } from "next/font/google";
 import "./globals.css";
 
 const dmMono = DM_Mono({
   variable: "--font-dm-mono",
   subsets: ["latin"],
   weight: "300"
+});
+
+const barriecito = Barriecito({
+  variable: "--font-barriecito",
+  subsets: ["latin"],
+  weight: "400"
+});
+
+const deliusSwashCaps = Delius_Swash_Caps({
+  variable: "--font-delius-swash-caps",
+  subsets: ["latin"],
+  weight: "400"
+});
+
+const eagleLake = Eagle_Lake({
+  variable: "--font-eagle-lake",
+  subsets: ["latin"],
+  weight: "400"
+});
+
+const gloriaHallelujah = Gloria_Hallelujah({
+  variable: "--font-gloria-hallelujah",
+  subsets: ["latin"],
+  weight: "400"
+});
+
+const jost = Jost({
+  variable: "--font-jost",
+  subsets: ["latin"],
+  weight: "400"
+});
+
+const barrio = Barrio({
+  variable: "--font-barrio",
+  subsets: ["latin"],
+  weight: "400"
 });
 
 export const metadata: Metadata = {
@@ -21,7 +57,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${dmMono.variable}`}>
+      <body className={`${dmMono.variable} ${barriecito.variable} ${deliusSwashCaps.variable} ${eagleLake.variable} ${gloriaHallelujah.variable} ${jost.variable} ${barrio.variable}`}>
         {children}
         <SpeedInsights />
       </body>
