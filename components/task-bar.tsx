e4fd5c8ed1034028d2870b40a4e554569647ebc5
Adham Shams <adham20011@gmail.com>
2025-07-23 23:50:32 +0300

    Finished start menu

--- components/task-bar.tsx
+++ components/task-bar.tsx
@@ -1,19 +1,62 @@
+"use client";
 import styles from "./task-bar.module.css";
 import Image from "next/image";
+import { useState } from "react";
 
 export default function TaskBar() {
+  const [showStartMenu, setShowStartMenu] = useState(false);
+
   return (
     <div className={styles.container}>
-        <div className={styles.startContainer}>
+        <div 
+          className={styles.startContainer}
+          onMouseEnter={() => setShowStartMenu(true)}
+          onMouseLeave={() => setShowStartMenu(false)}
+        >
           <Image src={"/sun.webp"} alt="sun" width={25} height={25} />
           <h2>Start</h2>
+          
+          {showStartMenu && (
+            <div className={styles.startMenu}>
+              <div className={styles.startMenuHeader}>
+                <div className={styles.userSection}>
+                  <Image src={"/user.jpg"} alt="Profile" width={48} height={48} className={styles.profileImage} />
+                  <div className={styles.userInfo}>
+                    <h3>Adham Shams</h3>
+                    <p>Full Stack Developer @ Botit</p>
+                  </div>
+                </div>
+              </div>
+              
+              <div className={styles.startMenuContent}>
+                <a href="mailto:hello@adhamshams.com" className={styles.menuItem}>
+                  <Image src={"/mail.png"} alt="Email" width={16} height={16} />
+                  <span>hello@adhamshams.com</span>
+                </a>
+                
+                <a href="/resume.pdf" className={styles.menuItem}>
+                  <Image src={"/word.png"} alt="Resume" width={16} height={16} />
+                  <span>Resume / CV</span>
+                </a>
+
+                <a href="https://www.linkedin.com/in/adhamshams" className={styles.menuItem}>
+                  <Image src={"/world.png"} alt="LinkedIn" width={16} height={16} />
+                  <span>LinkedIn Profile</span>
+                </a>
+                
+                <div className={styles.menuSeparator}></div>
+                
+                <a href="/off" className={styles.menuItem}>
+                  <Image src={"/off-icon.png"} alt="About" width={16} height={16} />
+                  <span>Turn Off Portfolio</span>
+                </a>
+              </div>
+            </div>
+          )}
         </div>
         <div className={styles.timeContainer}>
           <h2>
-            {new Date().toLocaleTimeString([], {
-              hour: "2-digit",
-              minute: "2-digit",
-            })}
+            13:22 PM
           </h2>
         </div>
       </div>
