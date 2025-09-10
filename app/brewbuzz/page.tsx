f1936680b4753b6aca971287a3158aa32af8ad82
Adham Shams <adham20011@gmail.com>
2025-09-10 03:47:17 +0300

    Added back button to case study

--- app/brewbuzz/page.tsx
+++ app/brewbuzz/page.tsx
@@ -3,8 +3,21 @@ import Image from "next/image";
 
 export default function BrewBuzz() {
     return (
-        <div>
-
+        <div className={styles.page}>
+            <div className={styles.header}>
+                <a href="/desktop">
+                    <Image
+                        src="/back.webp"
+                        alt="Back Button"
+                        width={40}
+                        height={40}
+                    />
+                </a>
+                <h2>Back</h2>
+            </div>
+            <div className={styles.hero}>
+                
+            </div>
         </div>
     );
 }
